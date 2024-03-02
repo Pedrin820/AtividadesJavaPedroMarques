d3f10c0c2108355e84d91ef937e9c700ebad68f3
Pedrin820 <pedromb03052005@gmail.com>
2024-03-01 21:04:23 -0300

    Atv1 finalizada

--- At1/src/Main.java
+++ At1/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira o seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Seja bem-vindo, " + nome + "!");
     }
 }
